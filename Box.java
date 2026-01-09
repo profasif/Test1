a65999be891e6bd6b62d68fb4c73366976b841dc
Dr. Asif R <asif.rampurawala@vsit.edu.in>
2026-01-09 11:46:13 +0530

    MyFirstTestCode

---
+++ Box.java
@@ -0,0 +1,25 @@
+public class Box {
+    private int length;
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    private int breadth;
+    public int getBreadth() {
+        return breadth;
+    }
+    public void setBreadth(int breadth) {
+        this.breadth = breadth;
+    }
+    private int height;
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+
+}
---
+++ Test1.java
@@ -0,0 +1,15 @@
+class Test1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hi");
+        Box box=new Box();
+        box.setLength(10);
+        box.setBreadth(20);
+        box.setHeight(30);  
+        System.out.println("Length: " + box.getLength());
+        System.out.println("Breadth: " + box.getBreadth());         
+        System.out.println("Height: " + box.getHeight());   
+            
+    }   
+}
